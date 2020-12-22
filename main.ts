387dfdebf56d2e7db4f2c134ba5bbb0980e8e493
patvelo <76450289+patvelo@users.noreply.github.com>
2020-12-22 21:54:11 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,15 +11,15 @@ scene.onOverlapTile(SpriteKind.Player, sprites.vehicle.roadVertical, function (s
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.vehicle.roadHorizontal, function (sprite, location) {
     tiles.setTileAt(location, sprites.dungeon.floorLight0)
-    tiles.setWallAt(tiles.getTileLocation(12, 4), false)
+    tiles.setWallAt(tiles.getTileLocation(12, 3), false)
 })
-tiles.setTilemap(tiles.createTilemap(hex`10001000080a0a03000000000000000000000000080a0a03000000000000000000000000080a0a02040404040404040404040000080a0a0a0a0a0a0a0a0a0a0a0c0a0000080a0a0a0a0a0a0a0a0a0a0a0c0a0000080b0b06090909070a0a060909090000080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a010000000000080a0a03000000080a0a010000000000080a0a03000000080a0a030000000000080a0a02040404050d0d030000000000080a0a0a0a0a0a0a0a0a030000000000080a0a0a0a0a0a0a0a0a03000000000009090909090909090909090000000000`, img`
+tiles.setTilemap(tiles.createTilemap(hex`10001000080a0a03000000000000000000000000080a0a03000000000000000000000000080a0a02040404040404040404040410080a0a0a0a0a0a0a0a0a0a0a0c0a0f01080a0a0a0a0a0a0a0a0a0a0a0c0a0f01080b0b06090909070a0a060909090e11080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a010000000000080a0a03000000080a0a010000000000080a0a03000000080a0a030000000000080a0a02040404050d0d030000000000080a0a0a0a0a0a0a0a0a030000000000080a0a0a0a0a0a0a0a0a03000000000012090909090909090909110000000000`, img`
+    2 2 2 2 . . . . . . . . . . . . 
     2 . . 2 . . . . . . . . . . . . 
-    2 . . 2 . . . . . . . . . . . . 
-    2 . . 2 2 2 2 2 2 2 2 2 2 2 . . 
-    2 . . . . . . . . . . . 2 . . . 
-    2 . . . . . . . . . . . 2 . . . 
-    2 2 2 2 2 2 2 2 . . 2 2 2 2 . . 
+    2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 . . . . . . . . . . . 2 . . 2 
+    2 . . . . . . . . . . . 2 . . 2 
+    2 2 2 2 2 2 2 2 . . 2 2 2 2 2 2 
     2 . . 2 . . . 2 . . 2 . . . . . 
     2 . . 2 . . . 2 . . 2 . . . . . 
     2 . . 2 . . . 2 . . 2 . . . . . 
@@ -30,7 +30,7 @@ tiles.setTilemap(tiles.createTilemap(hex`10001000080a0a0300000000000000000000000
     2 . . . . . . . . . 2 . . . . . 
     2 . . . . . . . . . 2 . . . . . 
     2 2 2 2 2 2 2 2 2 2 2 . . . . . 
-    `, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight0,sprites.vehicle.roadHorizontal,sprites.vehicle.roadVertical,myTiles.tile2], TileScale.Sixteen))
+    `, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight0,sprites.vehicle.roadHorizontal,sprites.vehicle.roadVertical,myTiles.tile2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.chestOpen,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerSouthWest], TileScale.Sixteen))
 let Läufer = sprites.create(img`
     ........................
     ........................
@@ -57,6 +57,6 @@ let Läufer = sprites.create(img`
     ........................
     ........................
     `, SpriteKind.Player)
-Läufer.setPosition(20, 10)
+Läufer.setPosition(25, 25)
 controller.moveSprite(Läufer, 47, 40)
 scene.cameraFollowSprite(Läufer)
