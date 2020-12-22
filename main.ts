d49c73e8b5b9028ffa892df11ca68a4e09eb7140
patvelo <76450289+patvelo@users.noreply.github.com>
2020-12-22 11:02:55 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,18 @@
+tiles.setTilemap(tiles.createTilemap(hex`100010000900010000000000000000000000000009000100000000000000000000000000090002040404040404040404040400000900000000000000000000000000000009000607070707070800060a0a0a0000090001000000000009000100000000000900010000000000090003000000000009000100000000000900030000000000090001000000000009000300000000000900010000000000090001000000000009000100000000000900010000000000090003000000000009000300000000000900030000000000090003000000000009000204040404040500030000000000090000000000000000000300000000000a0a0a0a0a0a0a0a0a0a0a0000000000`, img`
+    2 . 2 . . . . . . . . . . . . . 
+    2 . 2 . . . . . . . . . . . . . 
+    2 . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    2 . . . . . . . . . . . . . . . 
+    2 . 2 2 2 2 2 2 2 . 2 2 2 2 . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 . . . . . 2 . 2 . . . . . 
+    2 . 2 2 2 2 2 2 2 . 2 . . . . . 
+    2 . . . . . . . . . 2 . . . . . 
+    2 2 2 2 2 2 2 2 2 2 2 . . . . . 
+    `, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth0], TileScale.Sixteen))
