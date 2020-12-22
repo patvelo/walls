52ae11e91785816345aa2f09dc4126e6a666ce2f
patvelo <76450289+patvelo@users.noreply.github.com>
2020-12-22 22:40:22 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
     tiles.setTileAt(location, sprites.dungeon.floorLight0)
-    tiles.setWallAt(tiles.getTileLocation(1, 5), false)
+    Zufallszahl = randint(1, 2)
+    tiles.setWallAt(tiles.getTileLocation(Zufallszahl, 5), false)
     tiles.setTileAt(tiles.getTileLocation(8, 5), sprites.vehicle.roadHorizontal)
     tiles.setWallAt(tiles.getTileLocation(8, 5), true)
     tiles.setTileAt(tiles.getTileLocation(9, 5), sprites.vehicle.roadHorizontal)
@@ -11,24 +12,29 @@ scene.onOverlapTile(SpriteKind.Player, sprites.vehicle.roadVertical, function (s
 })
 scene.onOverlapTile(SpriteKind.Player, sprites.vehicle.roadHorizontal, function (sprite, location) {
     tiles.setTileAt(location, sprites.dungeon.floorLight0)
-    tiles.setWallAt(tiles.getTileLocation(12, 3), false)
+    Zufallszahl = randint(3, 4)
+    tiles.setWallAt(tiles.getTileLocation(12, Zufallszahl), false)
 })
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
+    game.over(true)
+})
+let Zufallszahl = 0
 tiles.setTilemap(tiles.createTilemap(hex`10001000080a0a03000000000000000000000000080a0a03000000000000000000000000080a0a02040404040404040404040410080a0a0a0a0a0a0a0a0a0a0a0c0a0f01080a0a0a0a0a0a0a0a0a0a0a0c0a0f01080b0b06090909070a0a060909090e11080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a030000000000080a0a03000000080a0a010000000000080a0a03000000080a0a010000000000080a0a03000000080a0a030000000000080a0a02040404050d0d030000000000080a0a0a0a0a0a0a0a0a030000000000080a0a0a0a0a0a0a0a0a03000000000012090909090909090909110000000000`, img`
     2 2 2 2 . . . . . . . . . . . . 
     2 . . 2 . . . . . . . . . . . . 
     2 . . 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    2 . . . . . . . . . . . 2 . . 2 
-    2 . . . . . . . . . . . 2 . . 2 
+    2 . 2 . . . . . 2 . . . 2 . . 2 
+    2 . . . 2 2 2 . . . 2 . 2 . . 2 
     2 2 2 2 2 2 2 2 . . 2 2 2 2 2 2 
     2 . . 2 . . . 2 . . 2 . . . . . 
+    2 . 2 2 . . . 2 . . 2 . . . . . 
     2 . . 2 . . . 2 . . 2 . . . . . 
+    2 2 . 2 . . . 2 . . 2 . . . . . 
     2 . . 2 . . . 2 . . 2 . . . . . 
+    2 . 2 2 . . . 2 . . 2 . . . . . 
+    2 . . 2 2 2 2 2 2 2 2 . . . . . 
+    2 2 . . . 2 . . . . 2 . . . . . 
     2 . . 2 . . . 2 . . 2 . . . . . 
-    2 . . 2 . . . 2 . . 2 . . . . . 
-    2 . . 2 . . . 2 . . 2 . . . . . 
-    2 . . 2 2 2 2 2 . 2 2 . . . . . 
-    2 . . . . . . . . . 2 . . . . . 
-    2 . . . . . . . . . 2 . . . . . 
     2 2 2 2 2 2 2 2 2 2 2 . . . . . 
     `, [myTiles.transparency16,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterNorth0,sprites.dungeon.floorLight0,sprites.vehicle.roadHorizontal,sprites.vehicle.roadVertical,myTiles.tile2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.chestOpen,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerSouthWest], TileScale.Sixteen))
 let Läufer = sprites.create(img`
@@ -60,3 +66,5 @@ let Läufer = sprites.create(img`
 Läufer.setPosition(25, 25)
 controller.moveSprite(Läufer, 47, 40)
 scene.cameraFollowSprite(Läufer)
+Zufallszahl = randint(8, 9)
+tiles.setWallAt(tiles.getTileLocation(Zufallszahl, 12), false)
